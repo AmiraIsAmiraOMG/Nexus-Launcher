6f3f10d6d7429f42cddb56a2833dc463b14f10bb
Wild Mastery Entertainment <167897093+AmiraIsAmiraOMG@users.noreply.github.com>
2024-12-17 21:15:33 +0000

    Initial commit

---
+++ Nexus Launcher/Screens/DllInjectionPage.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace Nexus_Launcher.Screens
+{
+    public partial class DllInjectionPage : UserControl
+    {
+        public DllInjectionPage()
+        {
+            InitializeComponent();
+            HelpLabel.Visible = false; // Hide the label
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DllInjectionPage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            try
+            {
+                // Open the GitHub link in the default browser
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = "https://github.com/max8447/Eon-Launcher",
+                    UseShellExecute = true // Required for URLs
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to open the link: " + ex.Message);
+            }
+        }
+
+        private void Help_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Check the current visibility and toggle it
+            if (HelpLabel.Visible)
+            {
+                HelpLabel.Visible = false; // Hide the label
+            }
+            else
+            {
+                HelpLabel.Visible = true; // Show the label
+            }
+        }
+
+        private void HelpLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Path to the config.json file
+            string configFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Injector", "config.json");
+
+            // Get the DLL URL from textBox1
+            string DLLDownload = textBox1.Text;
+
+            try
+            {
+                // Read the existing JSON content from the file
+                string jsonContent = File.ReadAllText(configFilePath);
+
+                // Deserialize the JSON to a C# object
+                var config = JsonConvert.DeserializeObject<Config>(jsonContent);
+
+                // If the config is null, initialize it
+                if (config == null)
+                {
+                    config = new Config
+                    {
+                        Download = new Download()
+                    };
+                }
+
+                // Update the DLLUrl value in the config object
+                if (config.Download != null)
+                {
+                    config.Download.DLLUrl = DLLDownload; // Set the DLL URL from textBox1
+                }
+
+                // Serialize the object back to JSON
+                string updatedJsonContent = JsonConvert.SerializeObject(config, Formatting.Indented);
+
+                // Write the updated JSON content back to the file
+                File.WriteAllText(configFilePath, updatedJsonContent);
+
+                MessageBox.Show("config.json updated successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error updating config.json: " + ex.Message);
+            }
+
+            // Start the Injector EXE (this line remains unchanged)
+            Process.Start("Injector\\Project Nexus GUI DLL INJECTOR.exe");
+        }
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string DLLDownload = textBox1.Text;
+        }
+
+        // Define a C# class to map the JSON structure
+        public class Config
+        {
+            public Download Download { get; set; }
+        }
+
+        public class Download
+        {
+            public string DLLUrl { get; set; }
+        }
+    }
+}
---
+++ Nexus Launcher/Screens/UpdatesPage.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Nexus_Launcher.Screens
+{
+    partial class UpdatesPage
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cascadia Code", 72F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(3, -13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(334, 127);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "NEXUS";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cascadia Code", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(19, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 35);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "beta";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cascadia Code", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(127, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(191, 35);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "SamIsSamOMG";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cascadia Code", 22.25F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(18, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(395, 40);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "NEXUS BETA IS HERE!!!";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Cascadia Code", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(19, 231);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(751, 210);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "WHOLE NEW USER INTERFACE!!!\r\n\r\nBACKEND SUPPORT FOR MULTIPLAYER DLLS (HELP NOT\r\nGI" +
+    "VEN USE AT OWN RISK)\r\n\r\n\r\n";
+            // 
+            // UpdatesPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "UpdatesPage";
+            this.Size = new System.Drawing.Size(985, 551);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
